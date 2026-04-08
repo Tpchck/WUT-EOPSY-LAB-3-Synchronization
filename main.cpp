86bf29d3962bf6b03c02bdc7f66209ab3e730a9a
kyakunin <01184959@pw.edu.pl>
2026-04-08 10:39:22 +0200

    chore: tidy up consumer exit logic comment

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
            no_sync ? "OFF" : "ON", use_delay ? "ON" : "OFF");
 
     pid_t pids[5];
-    const char types[] = {'A', 'B', 'C'};
+    char types[] = {'A', 'B', 'C'};
 
     for (int i = 0; i < 3; i++) {
         pids[i] = fork();
